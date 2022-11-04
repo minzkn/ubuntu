02203e30c01da966c8fb9174c9c3fe3c8eca31b0
JAEHYUK CHO <minzkn@minzkn.com>
2022-11-04 16:49:51 +0900

    multi platform build

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 
-#docker build --tag "hwport/ubuntu:www" .
-docker build --push --platform linux/amd64,linux/arm64/v8,linux/ppc64le,linux/s390 --tag "hwport/ubuntu:www" .
+#docker buildx build --push --platform linux/amd64,linux/arm64/v8,linux/ppc64le,linux/s390 --tag "hwport/ubuntu:www" .
+docker build --tag "hwport/ubuntu:www" .
