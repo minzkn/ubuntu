a12eefd15d47d3bd4a024fd3d46b46f8aca44498
JAEHYUK CHO <minzkn@minzkn.com>
2022-11-05 12:15:50 +0900

    multi platform support

--- build-docker-image.sh
+++ build-docker-image.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
 
+#docker buildx build --push --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x --tag "hwport/ubuntu:kvm" .
 docker build --tag "hwport/ubuntu:kvm" .
