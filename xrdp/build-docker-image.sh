16bd644385b9b7140d159f78c4ce958697de64ad
JAEHYUK CHO <minzkn@minzkn.com>
2022-11-11 14:11:49 +0900

    systemd fix ...

---
+++ xrdp/build-docker-image.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#docker buildx build --push --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x --tag "hwport/ubuntu:xrdp" .
+docker build --tag "hwport/ubuntu:xrdp" .
