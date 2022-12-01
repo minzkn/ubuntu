f0634659f9de018451b8c2d17ff804073150e5ad
JAEHYUK CHO <minzkn@minzkn.com>
2022-12-01 17:48:26 +0900

    xrdp some fix...

--- xrdp/__up.sh
+++ xrdp/__up.sh
@@ -4,4 +4,8 @@ g_docker_container_name=xrdp
 
 docker-compose up -d ${g_docker_container_name}
 
+echo 'you need follow command:'
+echo '$ docker exec -i -t "<container-name>" useradd -c "<comment>" -d "/home/<username>" -g "users" -G "users,adm,sudo" -s "/bin/bash" -m "<username>"'
+echo '$ docker exec -i -t "<container-name>" sh -c "echo \"<username>:<password>\" | chpasswd"'
+
 # End of __up.sh
