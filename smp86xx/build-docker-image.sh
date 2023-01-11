79e6516ee6d1bc791c8e19f30b2a8e63fe211176
JAEHYUK CHO <minzkn@minzkn.com>
2023-01-11 17:23:16 +0900

    autopoint add, 14.04 cmake3 update

--- smp86xx/build-docker-image.sh
+++ smp86xx/build-docker-image.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
 
 #docker buildx build --no-cache --push --platform linux/amd64 --tag "hwport/ubuntu:smp86xx" .
+#docker buildx build --no-cache --push --platform linux/amd64 --tag "hwport/sigmadesigns:smp86xx" .
+#docker buildx build --no-cache --push --platform linux/amd64 --tag "hwport/sigmadesigns:latest" .
 docker build --tag "hwport/ubuntu:smp86xx" .
