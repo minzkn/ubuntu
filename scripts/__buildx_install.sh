8046211fd618816418a22f4c0725d0e652d73afe
JAEHYUK CHO <minzkn@minzkn.com>
2023-01-20 21:26:33 +0900

    ...

--- scripts/__buildx_install.sh
+++ scripts/__buildx_install.sh
@@ -2,7 +2,7 @@
 
 DOCKER_BUILDKIT=1 docker build --platform=local -o . https://github.com/docker/buildx.git \
     && mkdir -p ~/.docker/cli-plugins \
-    && mv buildx ~/.docker/cli-plugins/docker-buildx \
+    && mv -f buildx ~/.docker/cli-plugins/docker-buildx \
     && docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
     && docker buildx create --name multiarch --driver docker-container --use \
     && docker buildx inspect --bootstrap \
