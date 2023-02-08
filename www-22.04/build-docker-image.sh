1e0765a7b7fe4cb088c151e9051ba477a186fb0b
JAEHYUK CHO <minzkn@minzkn.com>
2023-02-08 12:06:17 +0900

    new branch www-22.04 from www-latest (Ubuntu 22.04 bases + apache2 + php + ...)

---
+++ www-22.04/build-docker-image.sh
@@ -0,0 +1,4 @@
+#!/bin/sh
+
+#docker buildx build --no-cache --push --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x --tag "hwport/ubuntu:www-22.04" .
+docker build --tag "hwport/ubuntu:www-22.04" .
