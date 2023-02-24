3c788c6ffd0bf1143d641ed67f1bef8155d069a1
JAEHYUK CHO <minzkn@minzkn.com>
2023-02-24 17:06:48 +0900

    fix 20230224

--- strongswan-latest/build-docker-image.sh
+++ strongswan-latest/build-docker-image.sh
@@ -1,4 +1,5 @@
 #!/bin/sh
 
 #docker buildx build --no-cache --push --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x --tag "hwport/ubuntu:strongswan-latest" .
+#docker buildx build --no-cache --push --platform linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/s390x --tag "hwport/ubuntu:strongswan-latest" .
 docker build --tag "hwport/ubuntu:strongswan-latest" .
