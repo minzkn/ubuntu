c9a92978481e85028bd147ee9484f89b1ee830c7
JAEHYUK CHO <minzkn@minzkn.com>
2023-03-21 15:08:01 +0900

    hwport/ubuntu:22.04 , hwport/ubuntu:latest add xrdp with XRDP_DESKTOP=yes env

---
+++ latest/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+#   Copyright (C) MINZKN.COM
+#   All rights reserved.
+#
+#   Maintainers
+#     JaeHyuk Cho <mailto:minzkn@minzkn.com>
+#
+
+set -euo pipefail
+
+#HWPORT_USER_ID=$(id -u)
+#HWPORT_GROUP_ID=$(id -g)
+
+# === BEGIN : EDIT FOR YOU ===
+
+if [ "${XRDP_DESKTOP}" == "yes" ]
+then
+[ -f "/var/run/xrdp/xrdp-sesman.pid" ] && rm -f "/var/run/xrdp/xrdp-sesman.pid"
+[ -f "/var/run/xrdp/xrdp.pid" ] && rm -f "/var/run/xrdp/xrdp.pid"
+/usr/sbin/service xrdp start
+fi
+
+# === END   : EDIT FOR YOU ===
+
+exec "$@"
+
+# End of entrypoint.sh
