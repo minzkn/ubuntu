ddfa3757ef214a849ea6fd0ecd2867c844638c88
JAEHYUK CHO <minzkn@minzkn.com>
2024-09-06 23:03:10 +0900

    .

--- scripts/__buildx_install.sh
+++ scripts/__buildx_install.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-DOCKER_BUILDKIT=1 docker build --platform=local -o . https://github.com/docker/buildx.git \
-    && mkdir -p ~/.docker/cli-plugins \
-    && mv -f buildx ~/.docker/cli-plugins/docker-buildx \
-    && docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
+#DOCKER_BUILDKIT=1 docker build --platform=local -o . https://github.com/docker/buildx.git \
+#    && mkdir -p ~/.docker/cli-plugins \
+#    && mv -f buildx ~/.docker/cli-plugins/docker-buildx \
+#    && docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
+#    && docker buildx create --name multiarch --driver docker-container --use \
+#    && docker buildx inspect --bootstrap \
+#    && true
+
+docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
     && docker buildx create --name multiarch --driver docker-container --use \
     && docker buildx inspect --bootstrap \
     && true
