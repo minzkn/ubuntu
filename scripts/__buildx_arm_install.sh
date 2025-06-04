4296fc285a8040d1212b9d21879fcd1bfa25ee1e
JAEHYUK CHO <minzkn@minzkn.com>
2025-06-04 07:11:33 +0000

    .

---
+++ scripts/__buildx_arm_install.sh
@@ -0,0 +1,27 @@
+#!/bin/sh
+
+#DOCKER_BUILDKIT=1 docker build --platform=local -o . https://github.com/docker/buildx.git \
+#    && mkdir -p ~/.docker/cli-plugins \
+#    && mv -f buildx ~/.docker/cli-plugins/docker-buildx \
+#    && docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
+#    && docker buildx create --name multiarch --driver docker-container --use \
+#    && docker buildx inspect --bootstrap \
+#    && true
+
+# https://hub.docker.com/r/moby/buildkit
+ 
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
+#    && docker buildx create --name multiarch --driver docker-container --driver-opt "image=moby/buildkit:v0.20.0" --use \
+#    && docker buildx inspect --bootstrap \
+#    && true
+
+#docker run --rm --privileged multiarch/qemu-user-static --reset -p yes \
+#    && docker buildx create --name multiarch --driver docker-container --use \
+#    && docker buildx inspect --bootstrap \
+#    && true
+
+docker buildx create --name multiarch --driver docker-container --use \
+    && docker buildx inspect --bootstrap \
+    && true
+
+# End of __buildx_install.sh
