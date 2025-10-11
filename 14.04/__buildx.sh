6cfa69ffbf67a064bf797e3971096b7c879d0747
JAEHYUK CHO <minzkn@minzkn.com>
2025-10-11 16:12:13 +0900

    .

--- 14.04/__buildx.sh
+++ 14.04/__buildx.sh
@@ -1,4 +1,3 @@
 #!/bin/sh
 
-#docker buildx build --push --platform "linux/386,linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le" --tag "hwport/ubuntu:14.04" .
-docker buildx build --push --platform "linux/amd64" --tag "hwport/ubuntu:14.04" .
+docker buildx build --push --platform "linux/386,linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le" --tag "hwport/ubuntu:14.04" .
