91c6da61a132444307820b445a9058a2f998eef7
JAEHYUK CHO <minzkn@minzkn.com>
2026-04-28 00:59:53 +0900

    gc

--- deprecated/strongswan-latest/__buildx.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-
-docker buildx build --push --no-cache --platform "linux/amd64,linux/arm/v7,linux/arm64/v8,linux/ppc64le,linux/riscv64,linux/s390x" --tag "hwport/ubuntu:strongswan-latest" .
--- deprecated/strongswan-latest/__up.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-g_docker_container_name=strongswan-latest
-
-docker compose up -d ${g_docker_container_name}
-
-# End of __up.sh
